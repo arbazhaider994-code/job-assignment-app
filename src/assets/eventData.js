fdbc9d54f177eaf0140103a58ada4c795ad3c271
arbazhaider994-code <arbazhaider994@gmail.com>
2026-04-02 17:02:54 +0500

    Add files via upload

---
+++ src/assets/eventData.js
@@ -0,0 +1,54 @@
+import event1 from './event1.jpg'
+import event2 from './event2.png'
+import event3 from './event3.jpg'
+import event4 from './event4.png'
+import event5 from './event5.png'
+const eventData = [
+    {
+        img: event1,
+        name: "Property Sales Event",
+        date: "04 & 05  April 2026",
+        time: "10:00AM - 8:00 PM",
+        description: "Join an exciting property sales event in Islamabad offering exclusive discounts, real estate opportunities, expert guidance, and free entry at the prestigious Marriott Hotel venue.",
+        location: "Agha Khan Road, Marriott Hotel, Islamabad",
+        city: "Islamabad"
+    },
+    {   
+        img: event2,
+        name: "Biggest UK Eduction Expo",
+        date: "18 April",
+        time: "11:00AM to 7:00PM",
+        description: "Join Study Abroad Expo Islamabad on 18 April. Meet top universities, get scholarships & fast-track offers. Stop dreaming about the study abroad and start packing! Your global future is calling",
+        location: "Nishar Hotel, Gulberg III, Lahore",
+        city: "Lahore",
+
+    },
+    {
+        img:  event3,
+        name: "Spring Festival",
+        date: "4 & 5 April 2026",
+        time: "2:00PM - 11:00PM",
+        description: "Celebrate the vibrant spring festival with blooming flowers, cultural performances, traditional music, delicious food, colorful attire, and joyful gatherings that showcase the country’s rich heritage and lively spirit.",
+        location: "Sports Arena, Joyland, Rawalpindi",
+        city: "Rawalpindi",
+    },
+    {   
+        img: event4,
+        name: "Annual Technical Conference",
+        date: "5 Aril - 7 April",
+        time: "12:00AM to 8:00PM",
+        description: "Annual Technical Conference 2026 showcases innovation, sustainable energy solutions, and strategic growth, offering exhibition opportunities for brands to connect, collaborate, and expand globally.",
+        location: "Khayaban-e-Suhrwardy Rd opposite Convention Centre Serena Hotel, Islamabad.",
+        city: "Islamabad",
+    },
+    {
+        img: event5,
+        name: "LuckEnter New Product Lucnch Event",
+        date: "13 April 2026",
+        time: "7:00PM - 10:00PM",
+        description: "Join us for an exclusive evening dedicated to celebrating our product launch Event, groundbreaking innovation, and the strong partnerships that drive our success forward.",
+        location: "Aura Grande Event Complex, 2 Service Rd E, Gorr, Islamabad",
+        city: "Islamabad"
+    }
+]
+export default eventData;
